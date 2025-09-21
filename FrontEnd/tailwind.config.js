0f13a425d9dfe9c91b489253da76bd3ce4e7cbdd
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2025-09-21 12:13:58 +0700

    feat: FrontEnd app

---
+++ FrontEnd/tailwind.config.js
@@ -0,0 +1,17 @@
+import typography from "@tailwindcss/typography";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: "#0b1120",
+      },
+    },
+  },
+  plugins: [typography],
+};
